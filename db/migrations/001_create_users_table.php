cb5bf1a055be04561cab7b089e4b3233b422712e
Rossini Fonseca <rossinidrum@outlook.com>
2023-07-20 21:46:09 -0300

    cria migration e arquivo para executa-las

--- db/migrations/001_create_users_table.php
+++ db/migrations/001_create_users_table.php
@@ -1,19 +1,25 @@
 <?php
 
-require_once './db/conexao.php';
+require_once '../conexao.php';
+
 global $conexao;
 
-try {
-    $query = "CREATE TABLE users (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        nome VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
-        cpf VARCHAR(255) NOT NULL,
-        senha VARCHAR(255) NOT NULL)";
+function criarTabelaUsuarios($conexao)
+{
+    try {
+        $query = "CREATE TABLE IF NOT EXISTS users (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            nome VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            cpf VARCHAR(255) NOT NULL,
+            senha VARCHAR(255) NOT NULL
+        )";
 
 
-    $conexao->exec($query);
-    echo "Migração executada com sucesso.";
-} catch (PDOException $e) {
-    echo "Erro ao executar migração: " . $e->getMessage();
+
+        $conexao->exec($query);
+        echo "Tabela de usuários criada com sucesso!";
+    } catch (PDOException $e) {
+        echo "Erro ao executar migração: " . $e->getMessage();
+    }
 }
