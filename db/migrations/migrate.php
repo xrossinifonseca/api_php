ac69e6e6a32feaf5d399391c019275b13d337c20
Rossini Fonseca <rossinidrum@outlook.com>
2023-07-26 20:42:43 -0300

    remove tabela user e cria tabela cliente

--- db/migrations/migrate.php
+++ db/migrations/migrate.php
@@ -2,12 +2,10 @@
 
 
 require_once __DIR__ . '/001_create_users_table.php';
-require_once './002_make_email_and_cpf_unique.php';
 
 // Verifica se a conexão com o banco de dados está ativa
 if ($conexao) {
     criarTabelaUsuarios($conexao);
-    make_email_cpf_unique($conexao);
 } else {
     echo "Erro ao conectar ao banco de dados.";
 }
