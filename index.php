ee2eb9c13c779bdf25735ef2c64e4780f06b1dc6
Rossini Fonseca <rossinidrum@outlook.com>
2023-08-03 13:21:02 -0300

    adiciona composer

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
+
+
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 // header('Access-Control-Max-Age: 86400');
-
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
         header("Access-Control-Allow-Methods: POST, OPTIONS");
@@ -13,19 +15,57 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 }
 
 
-require_once "./models/ClienteModel.php";
-require_once './services/ClienteService.php';
-require_once './controllers/ClienteController.php';
-require_once "./db/conexao.php";
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 
-$clienteModel = new ClienteModel($conexao);
-$clienteService = new ClienteService($clienteModel, $conexao);
-$userController = new ClienteController($clienteService);
+date_default_timezone_set('America/Sao_Paulo');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/users') {
+use App\Controllers\AuthController;
+use App\Controllers\ClienteController;
+use App\Models\ClienteModel;
+use App\Services\Auth;
+use App\Services\ClienteService;
+use App\Db\Database;
 
-    return $userController->createUser();
+
+$db = new Database();
+$db->connect();
+
+
+
+
+
+$clienteModel = new ClienteModel();
+$clienteService = new ClienteService($clienteModel);
+$clienteController = new ClienteController($clienteService);
+$auth = new Auth();
+$authController = new AuthController($auth);
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/registrar') {
+    // $requestData = json_decode(file_get_contents("php://input"), true);
+
+
+    return $clienteController->createCliente();
+}
+
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_SERVER['REQUEST_URI'] === '/login') {
+
+    // $requestData = json_decode(file_get_contents("php://input"), true);
+
+    return $authController->loginCliente();
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === '/me') {
+
+    return $clienteController->getCliente();
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === '/') {
