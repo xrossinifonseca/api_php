74e161b6c85768853ee1c3999dc2b7dc6cf36ad9
Rossini Fonseca <rossinidrum@outlook.com>
2023-08-03 22:28:32 -0300

    adiciona classe CompraModel

---
+++ app/models/CompraModel.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use App\Db\Consulta;
+use Exception;
+use PDOException;
+
+class CompraModel
+{
+
+    private $consulta;
+
+    public function __construct()
+    {
+        $this->consulta = new Consulta();
+    }
+
+
+
+    public function cadastrarCompra($data)
+    {
+
+        try {
+            $this->consulta->insertInto('compra', $data);
+        } catch (PDOException $e) {
+            throw new Exception("Erro ao cadastrar compra " . $e->getMessage());
+        }
+    }
+
+    public function isCompraCadastrada($numero)
+    {
+        $result = $this->consulta->SelectWhere('compra', 'numero', $numero);
+        if ($result) {
+            throw new Exception("nota fiscal ja cadastrada.");
+            exit;
+        }
+    }
+
+    public function comprasPorDia($value)
+    {
+
+        $query = "SELECT COUNT(*) FROM compra WHERE cliente_id = :value AND DATE(data) = CURRENT_DATE()";
+        $stmt = $this->consulta->connect()->prepare($query);
+        $stmt->execute([
+            'value' => $value
+        ]);
+
+        $result = $stmt->fetchColumn();
+
+
+        return $result;
+    }
+}
